ffaab4fd01ea665d0de03837857c8cd6631bae49
MiseAI <dick@barbutonyc.com>
2025-06-16 22:04:58 -0400

    Delete backend directory

--- backend/models/restaurant.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from backend.database import Base
-
-class Restaurant(Base):
-    __tablename__ = "restaurants"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
