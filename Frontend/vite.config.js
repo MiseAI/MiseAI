37864db473bba5a477e8f3b95224257ca8145ad1
MiseAI <dick@barbutonyc.com>
2025-06-18 15:13:44 -0400

    Push

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
+  server: {
+    watch: {
+      // don’t watch dependencies or your dist folder for changes
+      ignored: ['**/node_modules/**', '**/dist/**']
+    }
   }
-});
+})
