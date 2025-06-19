0125e3712ebec3f3f69520b4adf062701330c679
MiseAI <dick@barbutonyc.com>
2025-06-19 12:23:35 -0400

    Push

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,12 @@
+# main.py
 from fastapi import FastAPI
 from routers import auth, users
 
 app = FastAPI(title="MiseAI API")
 
-app.include_router(auth.router)
-app.include_router(users.router)
+# mount your auth & users routers
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+app.include_router(users.router, prefix="/users", tags=["Users"])
 
 @app.get("/", tags=["Default"])
 def read_root():
