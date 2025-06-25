ab4963ede10486bc5aee77b391971b2f21672455
MiseAI <dick@barbutonyc.com>
2025-06-25 12:35:54 -0400

    12:35

---
+++ Frontend/src/App 2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+const App = () => (
+  <div>
+    <h1>MiseAI Platform</h1>
+    <Dashboard />
+  </div>
+);
+
+export default App;
