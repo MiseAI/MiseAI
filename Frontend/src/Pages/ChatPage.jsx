8ea8573517b9b7559157888ff673c89de9258f8e
MiseAI <dick@barbutonyc.com>
2025-06-27 10:55:53 -0400

    10:55

--- Frontend/src/Pages/ChatPage.jsx
+++ Frontend/src/Pages/ChatPage.jsx
@@ -1,14 +1,22 @@
+// src/pages/ChatPage.jsx
+import React from "react";
 import HistoryExportButton from "../components/HistoryExportButton";
 
-function ChatPage() {
-  const filter = ""; // eventually connected to a search bar
+const ChatPage = () => {
+  // If you’re implementing a search bar, you could wire it here.
+  // For now, we’ll leave filter as an empty string or some test value.
+  const filter = "";
+
   return (
-    <div>
-      <h1>MiseAI Chat Page</h1>
-      {/* Chat UI would go here */}
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Chat Page</h1>
+      <p>This is your chat page content.</p>
+      {/* Add your chat messages UI here */}
+
+      {/* Export button */}
       <HistoryExportButton filter={filter} />
     </div>
   );
-}
+};
 
 export default ChatPage;
