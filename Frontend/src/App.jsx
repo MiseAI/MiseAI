765545c4f00ac32282b5a3e353584a83e8f288be
MiseAI <dick@barbutonyc.com>
2025-06-27 11:12:17 -0400

    11:12

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
+// src/App.jsx
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ChatPage from "./pages/ChatPage";
+import ChatPage from "./pages/ChatPage.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/chat" element={<ChatPage />} />
+        {/* other routes */}
       </Routes>
     </BrowserRouter>
   );
