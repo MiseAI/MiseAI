9bd8c49de92db46c4aa092c3953c4345b758a1f2
MiseAI <dick@barbutonyc.com>
2025-06-27 17:22:40 -0400

    5:22

--- Backend/main.py
+++ Backend/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import auth
-from database import engine, Base
 from routes import history
 from routes import chat
+from database import engine, Base
+import os
 
 app = FastAPI()
 
 origins = [
     "https://frontend-production-f46d.up.railway.app",
+    "http://localhost:3000",  # helpful for local dev
 ]
 
 app.add_middleware(
@@ -24,3 +26,9 @@ Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(history.router, prefix="/api")
 app.include_router(chat.router, prefix="/api")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
