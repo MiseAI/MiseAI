788079f828d20171800c2113b19291226d391c61
MiseAI <dick@barbutonyc.com>
2025-06-28 14:40:34 -0400

    2:40

--- Backend/main.py
+++ Backend/main.py
@@ -32,6 +32,3 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
-
--from database import engine, Base
-+from database import engine, Base
